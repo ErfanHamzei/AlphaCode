ba706198b2c709dc48155b770b5fbadb8bf5dacc
ErfanHamzei <ehamzei40@gmail.com>
2021-01-20 11:07:18 +0330

    Box Shadow On links added

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 // define all UI variable
-const navToggler = document.querySelector('.nav-toggler');
-const navMenu = document.querySelector('.site-navbar ul');
-const navLinks = document.querySelectorAll('.site-navbar a');
+const navToggler = document.querySelector(".nav-toggler");
+const navMenu = document.querySelector(".site-navbar ul");
+const navLinks = document.querySelectorAll(".site-navbar a");
 
 // load all event listners
 allEventListners();
@@ -9,20 +9,30 @@ allEventListners();
 // functions of all event listners
 function allEventListners() {
   // toggler icon click event
-  navToggler.addEventListener('click', togglerClick);
+  navToggler.addEventListener("click", togglerClick);
   // nav links click event
-  navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
+  navLinks.forEach((elem) => elem.addEventListener("click", navLinkClick));
 }
 
 // togglerClick function
 function togglerClick() {
-  navToggler.classList.toggle('toggler-open');
-  navMenu.classList.toggle('open');
+  navToggler.classList.toggle("toggler-open");
+  navMenu.classList.toggle("open");
 }
 
 // navLinkClick function
 function navLinkClick() {
-  if(navMenu.classList.contains('open')) {
+  if (navMenu.classList.contains("open")) {
     navToggler.click();
   }
 }
+
+//change header background to black
+navLinks.forEach((link) => {
+  link.addEventListener("mouseenter", () => {
+    document.querySelector("header").style.backgroundColor = "#000";
+  });
+  link.addEventListener("mouseout", () => {
+    document.querySelector("header").style.backgroundColor = "rgba(0, 0, 0, 0.692)";
+  });
+});
